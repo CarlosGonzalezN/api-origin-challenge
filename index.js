c474fd014755cf34bfd1c10c3f05b4d19e361f79
CarlosGonzalez <carlogonzalez@alumno.unlam.edu.ar>
2023-06-15 20:25:44 -0300

    fix: add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 const port = process.env.PORT;
@@ -8,7 +9,7 @@ const userRoutes = require("./src/routers/users/index");
 const stocksRouter = require("./src/routers/stocks");
 // Middleware para procesar datos en formato JSON
 app.use(express.json());
-
+app.use(cors());
 // Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port}`);
